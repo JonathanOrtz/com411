26f7e5136751b9528c76da1b170edfb097a82657
repl.it user <replituser@example.com>
2020-10-19 04:17:19 +0000

    Added code to demonstrate the use of comparison operators

---
+++ Basics/decision/simple_decision/comparison_operators.py
@@ -0,0 +1,12 @@
+# asking for the first number
+print("Please introduce the firstnumber")
+first = int(input())
+print("Please introduce the secondnumber")
+second = int(input())
+
+if (first>second):
+  print("The first number is the biggest")
+elif(first<second):
+  print("The first number is the smallest")
+else:
+  print("The first and the second number are equal")
--- Basics/decision/simple_decision/modulo_operator.py
+++ Basics/decision/simple_decision/modulo_operator.py
@@ -1,7 +1,7 @@
 #asking for a whole number
 print("Please enter a whole number.")
 number = int(input())
-
+# use 2 to know if it is a odd number 
 if(number%2==1):
   print("The number {} is an odd number". format(number))
 else:
