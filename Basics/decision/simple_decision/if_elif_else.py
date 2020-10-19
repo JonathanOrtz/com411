6b2afdbae4d7755d9fa16ec58f1b8b60d7c761d1
repl.it user <replituser@example.com>
2020-10-19 03:58:28 +0000

    Added code to demonstrate a module operator

---
+++ Basics/decision/simple_decision/if_elif_else.py
@@ -0,0 +1,13 @@
+# asking for what direction should we take
+print("Towars which direction should I paint(up, down, left or right)?")
+direction = input()
+if (direction == "up"):
+  print("\nI am painting in the upward direction!")
+elif(direction == "down"):
+  print("I am painting in the downward direction!")
+elif(direction == "left"):
+  print("\nI am painting in the leftward direction!")
+elif(direction == "right"):
+  print("\nI am painting in the rightward direction!")
+else:
+  print("\nI do not recognise that direction")
---
+++ Basics/decision/simple_decision/modulo_operator.py
@@ -0,0 +1,8 @@
+#asking for a whole number
+print("Please enter a whole number.")
+number = int(input())
+
+if(number%2==1):
+  print("The number {} is an odd number". format(number))
+else:
+  print("The Number {} is an even number". format(number))
