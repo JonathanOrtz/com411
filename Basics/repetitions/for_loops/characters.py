4b10853e355d1a9f6c9e4fead605a2f756294ba1
repl.it user <replituser@example.com>
2020-11-07 03:38:50 +0000

    new code to explain the steps

--- Basics/repetitions/for_loops/characters.py
+++ Basics/repetitions/for_loops/characters.py
@@ -3,3 +3,7 @@ strange_markings=input()
 print("Identifying...")
 for markings in range(0,len(strange_markings),1):
   print("Index",markings,":",strange_markings[markings])
+
+# in this case count is markings
+# we use markings to print it in the print function
+# we use markings to print the strange_markigns neatly way
