2e1de6278cc28bd462f061cb8b54d97d975f9610
repl.it user <replituser@example.com>
2020-11-08 18:53:42 +0000

    Added code to demonstrate the use of built-in functions: abs, range, chr.

---
+++ Basics/functions/ascii_character.py
@@ -0,0 +1,7 @@
+print("Program started")
+ascii_code=abs(int(input("Please enter an ASCII code:")))
+
+for ride in range(32,127,1):
+  if ascii_code==ride:
+    chr(ascii_code)
+    print(f"The character represented by the ASCII code {ascii_code} is {chr(ride)}.")
