1c347836cdee5ed7e608623f0987f8899d666891
repl.it user <replituser@example.com>
2020-11-09 02:17:28 +0000

    Added code to demonstrate the use of a user-defined function with a loop.

---
+++ Basics/functions/function_with_loop.py
@@ -0,0 +1,11 @@
+def cross_bridge (steeps):
+  #steeps in this part is equal to 5 because the valor given to the function is 5
+    for steep in range(steeps):
+       print("Crossed step")
+  #steeps in this part is equal to 5
+    if steeps>5:
+       print("The bridge is collapsing!")
+    else:
+       print("we must keep going!")
+    
+cross_bridge(5) #valor of the function
