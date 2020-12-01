d1ead33e953f2442a003da155e291b2728ac7af0
repl.it user <replituser@example.com>
2020-12-01 11:43:05 +0000

    Updated classes human and robot with methods to display objects.

--- objects/oop/human.py
+++ objects/oop/human.py
@@ -10,7 +10,15 @@ class Human:
   #  An instance method (to display)
   def display(self):
     print(f"I am {self.name}")
+  # Instance method repr(to return an formal string representation of the object)
+  def __repr__(self):
+    return f'robot(name={self.name}, age={self.age}, energy{Human.MAX_ENERGY})'
+  # Instance method str(to return an informal sring of the object)
+  def __str__(self):
+    return f'My name is {self.name} and I am {self.age} and my energy is {Human.MAX_ENERGY}'
 
 if (__name__ == "__main__"):
   human = Human()
   human.display()
+  print(human)
+  print(repr(human))
